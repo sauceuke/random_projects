a9b29e516f4bc8abdedbc736f68d5fa3148db9b6
India ✨ <101438418+sauceuke@users.noreply.github.com>
2022-09-15 03:17:25 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const PORT = 8000
+const express = require('express')
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+const app = express()
+
+const articles = []
+
+app.get('/', (req,res) => {
+    res.json('Welcome to my Climate Change News API')
+})
+
+app.get('/news', (req,res) => {
+
+    axios.get('https://www.theguardian.com/environment/climate-crisis')
+        .then((response) => {
+            const html = response.data
+            const $ = cheerio.load(html)
+
+            $('a:contain("climate")', html),each(function () {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+                articles.push({
+                    title,
+                    url,
+                })
+            })
+            res.json(articles)
+        }).catch((err) => console.log(err))
+
+})
+
+app.listen(PORT, () => console.log('server running on PORT ${PORT}'))
